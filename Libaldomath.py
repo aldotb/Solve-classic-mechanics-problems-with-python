b81e5f3b9d41d5ff672e1284c9031fac4ef27e00
Aldo Tamariz <aldotb@gmail.com>
2024-02-12 01:34:47 -0500

    Add files via upload

---
+++ Libaldomath.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.insert(0, 'Libaldo/')
+
+from sympy import *
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import Image, display
+
+
+
+from lib_Variables import *
+from lib_Mathematica import *
+from lib_Algorith import * 
+from lib_Mathbasic import *
+from lib_tools import *
+
+
+from  lib_MyEq import *
+from  lib_MyEqEq import *
+from  lib_MyIntegral import *
+from  lib_MyDiff import *
+
+from lib_Physics import *
+ 
+ 
+init_printing() 
+ 
+ 
